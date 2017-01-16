3e578d37961de9a1d04c7d97916e4740295b39ba
lunlun1992 <lunlun1992@163.com>
2017-01-16 21:45:08 +0800

    add new files

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,21 @@
+//向hashmap里面放数，直到找到为止，输出这两个index。
+//注意这样可以避免重复的数字
+//{}初始化不失为一种好的方法。
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ret;
+        int len = nums.size();
+        for(int i = 0; i < len; i++)
+        {
+            int another = target - nums[i];
+            if(0 != s.count(another))
+                return {s[another], i};
+            s[nums[i]] = i;
+        }
+        return ret;
+    }
+private:
+    unordered_map<int, int> s;
+};
---
+++ 2. Add Two Numbers Add to List.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//最好使用||，原因是代码会更简单。
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        ListNode H(0);
+        int adder = 0;
+        ListNode *h = &H;
+        while(l1 || l2)
+        {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = x + y + adder;
+            h->next = new ListNode(sum);
+            h = h->next;
+            adder = h->val / 10;
+            h->val = h->val > 9 ? h->val - 10 : h->val;
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+        }
+        if(adder)
+            h->next = new ListNode(adder);
+        return H.next;
+    }
+};
